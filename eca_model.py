773842d4db16c2b66efd143b4d48cead9a0fb385
Jan <jan.oechsler+github@gmail.com>
2022-02-07 17:06:44 +0100

    Start MVC design pattern

---
+++ eca_model.py
@@ -0,0 +1,12 @@
+class ECA_Model:
+    """
+    Object holding all data for the pyECA
+    """
+
+    def __init__(self):
+        """
+        Default Constructor.
+        """
+        # TODO: add better doc
+
+        pass
